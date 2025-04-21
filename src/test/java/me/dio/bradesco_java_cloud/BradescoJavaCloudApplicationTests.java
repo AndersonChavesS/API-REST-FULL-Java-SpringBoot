7d83301394f518532d9143f59be10da0dd1e9b9f
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-04-21 20:00:49 -0300

    docs: Java RESTfull API criada durante o bootcamp Bradesco Java Cloud

---
+++ src/test/java/me/dio/bradesco_java_cloud/BradescoJavaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bradesco_java_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoJavaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
